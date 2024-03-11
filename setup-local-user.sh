9791a417bbffec4dc5ca0a9e23900949dd37e87f
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-11 09:45:55 +0000

    feature: exiting script if user is not sudo or root

--- setup-local-user.sh
+++ setup-local-user.sh
@@ -11,7 +11,12 @@
 # You will be prompted to enter the username (login), the person name, and a password.
 # The username, password, and host for the account will be displayed.
 
-# Make sure the script is being executed with superuser privileges, run: `sudo ./setup-local-user.sh`
+# Make sure the script is being executed with superuser privileges
+if [[ "$UID" -ne 0 ]]
+then
+  echo "Run script as sudo or root."
+  exit 1
+fi
 
 # Get the username (login).
 
