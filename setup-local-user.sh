c866f7bf99f6996459ad5c1f5654e3519eeaba49
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-11 09:33:08 +0000

    docs: script goal and how to run it on a linux machine

--- setup-local-user.sh
+++ setup-local-user.sh
@@ -1,58 +1,25 @@
 #!/bin/bash
-#
+
+#Goal:
+# The goal of this exercise is to create a shell script that adds users to the same Linux system as the script is executed on.
+
+# Scenario:
+# Imagine that you're working as a Linux System Administrator for a fast growing company. The latest company initiative requires you to build and deploy dozens of servers. You're falling behind schedule and are going to miss your deadline for these new server deployments because you are constantly being interrupted by the help desk calling you to create new Linux accounts for all the people in the company who have been recruited to test out the company's newest Linux-based application.
+# In order to meet your deadline and keep your sanity, you decide to write a shell script that will create new user accounts. Once you're done with the shell script you can put the help desk in charge of creating new accounts which will finally allow you to work uninterrupted and complete your server deployments on time.
+
 # This script creates a new user on the local system.
 # You will be prompted to enter the username (login), the person name, and a password.
 # The username, password, and host for the account will be displayed.
 
-# Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" -ne 0 ]]
-then
-   echo 'Please run with sudo or as root.'
-   exit 1
-fi
+# Make sure the script is being executed with superuser privileges, run: `sudo ./setup-local-user.sh`
 
 # Get the username (login).
-read -p 'Enter the username to create: ' new_username
 
 # Get the real name (contents for the description field).
-read -p 'Enter the name of the person or application that will be using this account: ' comment
-
 # Get the password.
-read -p 'Enter the password to use for the account: ' new_password
-
-# Create the account.
-useradd -c "${comment}" -m "${new_username}"
-
+# Create the user with the password.
 # Check to see if the useradd command succeeded.
-# We don't want to tell the user that an account was created when it hasn't been.
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'The account could not be created.'
-  exit 1
-fi
-
 # Set the password.
-echo "${new_password}" | passwd --stdin "${new_username}"
-
 # Check to see if the passwd command succeeded.
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'The password for the account could not be set.'
-  exit 1
-fi
-
 # Force password change on first login.
-passwd -e "${new_username}"
-
 # Display the username, password, and the host where the user was created.
-echo
-echo 'username:'
-echo "${new_username}"
-echo
-echo 'password:'
-echo "${new_password}"
-echo
-echo 'host:'
-echo "${HOSTNAME}"
-exit 0
-
