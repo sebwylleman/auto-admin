a13ad2f159caba4191445609414d150b075e3c4c
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-12 11:07:25 +0000

    file description

--- auto-setup-local-user.sh
+++ auto-setup-local-user.sh
@@ -1,67 +1,30 @@
-#!/bin/bash
-#
-# This script creates a new user on the local system.
-# You must supply a username as an argument to the script.
-# Optionally, you can also provide a comment for the account as an argument.
-# A password will be automatically generated for the account.
-# The username, password, and host for the account will be displayed.
+# Goal:
+# The goal of this exercise is to create a shell script that adds users to the same Linux system as the script is executed on.
 
-# Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" -ne 0 ]]
-then
-   echo 'Please run with sudo or as root.' >&2
-   exit 1
-fi
+# Scenario:
+# The help desk team has been using the "add-local-user.sh" script you created.  They're really happy that they don't have to wait on you to create new accounts. :-)  However, they would like for you to make a couple of changes to the script when you get a chance.
 
-# If the user doesn't supply at least one argument, then give them help.
-if [[ "${#}" -lt 1 ]]
-then
-  echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
-  echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.' >&2
-  exit 1
-fi
+# They're tired of coming up with a unique password for each user they create.  As a matter of fact, Jim keeps using "password" as the password for every account.  They think it would be great if the script automatically generated a password for each new account.  That way Jim and the rest of the team won't have to even think about passwords any longer.
 
-# The first parameter is the user name
-USER_NAME="${1}"
+# Also, they think it would be a little more efficient if they could just specify the account name and account comments on the command line instead of being prompted for them.  They already know what they are going to type so they would just rather type it all in at one time.
 
-# The rest of the parameters are for the account comments.
-shift
-COMMENT="${@}"
+# Since you're happy that you're not the one creating all the new accounts any longer, you decide to accommodate their requests.  (You're so nice!)
 
-# Generate a password.
-PASSWORD=$(date +%s%N | sha256sum | head -c48)
+# Shell Script Requirements:
+# You have your requirements from the "add-local-user.sh" script you created.  You decide to use those as the basis for your new requirements.  You come up with the following list.
 
-# Create the user with the password.
-useradd -c "${COMMENT}" -m ${USER_NAME} &> /dev/null
+# The script:
 
-# Check to see if the useradd command succeeded.
-# We don't want to tell the user that an account was created when it hasn't been.
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'The account could not be created.' >&2
-  exit 1
-fi
+# Is named "add-new-local-user.sh".  (You add the word new to distinguish it from the original script.)
 
-# Set the password.
-echo ${PASSWORD} | passwd --stdin ${USER_NAME} &> /dev/null
+# Enforces that it be executed with superuser (root) privileges.  If the script is not executed with superuser privileges it will not attempt to create a user and returns an exit status of 1.
 
-# Check to see if the passwd command succeeded.
-if [[ "${?}" -ne 0 ]]
-then
-  echo 'The password for the account could not be set.' >&2
-  exit 1
-fi
+# Provides a usage statement much like you would find in a man page if the user does not supply an account name on the command line and returns an exit status of 1.
 
-# Force password change on first login.
-passwd -e ${USER_NAME} &> /dev/null
+# Uses the first argument provided on the command line as the username for the account.  Any remaining arguments on the command line will be treated as the comment for the account.
 
-# Display the username, password, and the host where the user was created.
-echo 'username:'
-echo "${USER_NAME}"
-echo
-echo 'password:'
-echo "${PASSWORD}"
-echo
-echo 'host:'
-echo "${HOSTNAME}"
-exit 0
+# Automatically generates a password for the new account.
+
+# Informs the user if the account was not able to be created for some reason.  If the account is not created, the script is to return an exit status of 1.
+
+# Displays the username, password, and host where the account was created.  This way the help desk staff can copy the output of the script in order to easily deliver the information to the new account holder.
