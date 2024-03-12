3d40c96f594c8338c42c888bdba8d81301a1ebc8
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-12 14:21:56 +0000

    fix: useradd command bug fix

--- auto-setup-local-user.sh
+++ auto-setup-local-user.sh
@@ -22,6 +22,59 @@ fi
 # Reminds the user to provide an account name as an argument to this script
 if [[ "$#" -lt 1 ]]
 then
+        echo "Usage: sudo $0 USERNAME [USERNAME]..."
+        exit 1
+fi
+
+# Uses the first argument provided on the command line as the username for the account.
+USERNAME=$1
+
+# Any remaining arguments on the command line will be treated as the comment for the account.
+shift
+COMMENT=$@
+
+# Generate random a special character out of the string below:
+SPECIAL_CHAR=$(echo "!@$%^&*=_-()+?/><" | fold -w1 | shuf | head -c1)
+
+# Automatically generates a strong password for the new account.
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c21)$SPECIAL_CHAR
+
+# Creates the user account, flag if unsuccesful
+
+useradd -c "$COMMENT" -m "$USERNAME"
+if [[ "$?" -ne 0 ]]
+then
+        echo "Account could not be created."
+        exit 1
+fi
+
+# Sets the user's password.
+echo $PASSWORD | passwd --stdin $USERNAME
+
+# Verifies successful password creation for the new user.
+if [[ "$?" -ne 0 ]]
+then
+        echo "Unsuccesful password creation for $USERNAME."
+        exit 1
+fi
+
+# Enforces password change at first login.
+passwd -e $USERNAME
+
+# Displays user information (username, password, host).
+echo "username: $USERNAME"
+echo "password: $PASSWORD"
+echo "host: $HOSTNAME"
+exit 0
+if [[ "$UID" -ne 0 ]]
+then
+        echo "Usage: must have superuser or root provileges to execute script."
+        exit 1
+fi
+
+# Reminds the user to provide an account name as an argument to this script
+if [[ "$#" -lt 1 ]]
+then
         echo "Usage: sudo $0 USERNAME [COMMENT]..."
         exit 1
 fi
