ec8e34ed408b1d8af280880891ec1154a9e644fc
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-18 10:14:06 +0000

    feature: checks for sudo or root privileges to run succesfully

--- disable-user.sh
+++ disable-user.sh
@@ -3,9 +3,9 @@
 # This script allows management of user accounts on the local system.
 # It offers options for disabling, deleting, and potentially archiving users.
 
-# Enforces script to be run only by superuser (sudo) privileges
+# Enforces script to be run only by the superuser or root account.
 if [[ "$UID" -ne 0 ]]
 then
-        echo 'Usage: Required sudo privileges to run script' >&2
+        echo 'Usage: Required sudo or root privileges to run script' >&2
         exit 1
 fi
