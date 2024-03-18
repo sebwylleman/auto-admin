44b9cc7a002416c44046bfa9be0aa05774de606e
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-18 10:48:40 +0000

    feature:  usage function declaration

--- disable-user.sh
+++ disable-user.sh
@@ -9,3 +9,14 @@ then
         echo 'Usage: Required sudo or root privileges to run script' >&2
         exit 1
 fi
+
+# Display the usage then exit.
+usage() {
+                echo "Usage: $0 [-dra] USER [USER2]..." >&2
+                echo "Delete a local user account and related files." >&2
+                echo "-d Deletes accounts." >&2
+                echo "-r Removes the account's home directory." >&2
+                echo "-a Creates an archive of the account's home directory." >&
+2
+                exit 1
+}
