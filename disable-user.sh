22f7cdaf0a7360a1696d635e4975636c0974bf61
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-18 14:05:41 +0000

    featrue: archiving the user's directory

--- disable-user.sh
+++ disable-user.sh
@@ -46,27 +46,46 @@ fi
 # Loop through all the usernames supplied as arguments.
 for USER in $@
 do
-        echo "Processing user: "$USERNAME"
+    echo "Processing user: "$USERNAME"
 
-        # Make sure the UID of the account is at least 1000 so that we do not remove any system accounts.
-        USERID=$(id -u ${USER})
-        if [[ "${USERID}" -lt 1000 ]]
+    # Make sure the UID of the account is at least 1000 so that we do not remove any system accounts.
+    USERID=$(id -u ${USER})
+    if [[ "${USERID}" -lt 1000 ]]
+    then
+        echo "Refusing to remove the $USER account with UID ${USERID}." >&2
+        exit 1
+    fi
+
+    # Create an archive if requested to do so.
+    if [[ "${ARCHIVE}" = 'true']]
+    then
+
+        # Make sure the ARCHIVE_DIR directory exists.
+        if [[ ! -d "${ARCHIVE_DIR}" ]]
         then
-            echo "Refusing to remove the $USER account with UID ${USERID}." >&2
-            exit 1
+            echo "Creating ${ARCHIVE_DIR} directory."
+            mkdir -p ${ARCHIVE_DIR}
+            if [[ "${?}" -ne 0 ]]
+            then
+                echo "The archive directory ${ARCHIVE_DIR} could not be created." >&2
+                exit 1
+            fi
         fi
 
-        # Create an archive if requested to do so.
-        if [[ "${ARCHIVE}" = 'true']]
+        # Archive the user's home directory and move it into the ARCHIVE_DIR
+        HOME_DIR="/home/${USER}"
+        ARCHIVE_FILE= "${ARCHIVE_DIR}/${USER}.tgz"
+        if [[ -d "${HOME_DIR}" ]]
         then
-            # Make sure the ARCHIVE_DIR directory exists.
-            if [[ ! -d "${ARCHIVE_DIR}" ]]
+            echo "Archiving ${HOME_DIR} to ${ARCHIVE_FILE}"
+            tar -zcf ${ARCHIVE_FILE} ${HOME_DIR} &> /dev/null
+            if [[ "${?} -ne 0 ]]
             then
-                echo "Creating ${ARCHIVE_DIR} directory."
-                mkdir -p ${ARCHIVE_DIR}
-                if [[ "${?}" -ne 0 ]]
-                then
-                    echo "The archive directory ${ARCHIVE_DIR} could not be created." >&2
-                    exit 1
-                fi
+                echo "Could not create ${ARCHIVE_FILE}". >&2
+                exit 1
             fi
+        else
+            echo "${HOME_DIR} does not exist or is not a directory." >&2
+            exit 1
+        fi
+    fi
