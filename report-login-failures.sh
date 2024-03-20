aabe718c890733a179194b8504685a5266b771be
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-20 13:49:16 +0000

    init: project initialisation

---
+++ report-login-failures.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# This script requires a file to read as an argument.
+# It counts the number of failed login attempts by IP address and produces output in CSV format.
+# Displays: any IP addresses with more than 10 failed login attempts, the number of attempts made, the IP address from which
+# those attempts were made, and the location of the IP address.
+
