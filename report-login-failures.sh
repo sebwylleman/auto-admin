889a3726ce49abc65318c0785805701554af1104
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-20 17:36:33 +0000

    feature: loop through the list of failed attempts and display count, IP and location if over limit.

--- report-login-failures.sh
+++ report-login-failures.sh
@@ -3,7 +3,7 @@
 # Analyse Login Attempts Report for a CENTOS system. MUST HAVE SUPERUSER OR ROOT PRIVILEGES to display contents
 
 # This script analyses a log file containing failed login attempts and generates a report summarising potentially suspicious activity. 
-# If there are any IPs with over LIMIT failures, display the count, IP, and location.
+# If there are any IPs with over max login limit failures, display the count, IP, and location.
 
 MAX_LOGIN_ATTEMPTS='10'
 LOG_FILE="${1}"
@@ -14,3 +14,15 @@ then
     echo "Error: Please provide a log file path as an argument." >&2
     exit 1
 fi
+
+# Loop through the list of failed attempts and corresponding IP addresses.
+grep Failed ${LOG_FILE} | awk '{print $(NF - 3)}' | sort | uniq -c | sort -nr |  while read COUNT IP
+do
+  # If the number of failed attempts is greater than the limit, display count, IP, and location.
+  if [[ "${COUNT}" -gt "${MAX_LOGIN_ATTEMPTS}" ]]
+  then
+    LOCATION=$(geoiplookup ${IP} | awk -F ', ' '{print $2}')
+    echo "${COUNT},${IP},${LOCATION}"
+  fi
+done
+exit 0
