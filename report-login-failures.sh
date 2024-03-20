5aeadc26874e6c369430402f06c9d4da28a554dd
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-03-20 13:55:59 +0000

    init: instructions and pseudocode added

--- report-login-failures.sh
+++ report-login-failures.sh
@@ -1,7 +1,23 @@
 #/bin/bash
 
-# This script requires a file to read as an argument.
-# It counts the number of failed login attempts by IP address and produces output in CSV format.
-# Displays: any IP addresses with more than 10 failed login attempts, the number of attempts made, the IP address from which
-# those attempts were made, and the location of the IP address.
+# Analyze Login Attempts Report
 
+# This script analyses a log file containing failed login attempts and generates a report summarising potentially suspicious activity. 
+#It helps identify IP addresses exhibiting an unusually high number of failed login attempts, which could be indicative of brute-force attacks or unauthorised access attempts.
+
+# The script outputs a report in CSV format (Comma-Separated Values) with the following columns:
+
+# Count: Number of failed login attempts from a specific IP address.
+# IP: The IP address associated with the failed login attempts.
+# Location: (Optional, depending on your log data) Geographic location associated with the IP address (may require additional tools or configuration).
+
+# How to Use:
+# Save this script as report-login-failures.sh (or your preferred name).
+# Make the script executable using chmod +x report-login-failures.sh
+# Run the script with the path to your login attempt log file as an argument:
+
+# Check if a log file was supplied as an argument.
+# Display the CSV header.
+# Loop through the list of failed attempts and corresponding IP addresses.
+# If the number of failed attempts is greater than the limit, display
+# count, IP, and location.
