a7a8c48b6649b5a42abd47f6c2203a4bd2822c0f
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2024-04-10 14:51:28 +0100

    docs: concise comments

--- setup-local-user.sh
+++ setup-local-user.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-#Goal:
-# The goal of this exercise is to create a shell script that adds users to the same Linux system as the script is executed on.
-
-# Scenario:
-# Imagine that you're working as a Linux System Administrator for a fast growing company. The latest company initiative requires you to build and deploy dozens of servers. You're falling behind schedule and are going to miss your deadline for these new server deployments because you are constantly being interrupted by the help desk calling you to create new Linux accounts for all the people in the company who have been recruited to test out the company's newest Linux-based application.
-# In order to meet your deadline and keep your sanity, you decide to write a shell script that will create new user accounts. Once you're done with the shell script you can put the help desk in charge of creating new accounts which will finally allow you to work uninterrupted and complete your server deployments on time.
-
-# This script creates a new user on the local system.
+# This script creates a new user then adds the user to the same Linux system as the script is executed on.
 # You will be prompted to enter the username (login), the person name, and a password.
 # The username, password, and host for the account will be displayed.
 
